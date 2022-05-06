5221baa743c4e9ffaae7f025e1505990a04d6008
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-05-06 02:46:11 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+function formulario(){
+  document.getElementById('criar-topico').style.display='none';
+  document.getElementById('enviar-topico').style.display='block';
 }
+function formulario2(){
+  document.getElementById('enviar-topico').style.display='none';
+  document.getElementById('criar-novo-topico').style.display='block';
+  document.getElementById('feedback').style.display='block';
+}
+function formulario3(){
+  document.getElementById('enviar-topico').style.display='none';
+  document.getElementById('feedback').style.display='none';
+  document.getElementById('enviar-topico').style.display='block';
+}
+function formularioComentario(){
+  var comentario=document.getElementById('comentario');
+  var btn_icon=document.getElementById('btn-icon');
+  var btn_icon_2=document.getElementById('btn-icon-2');
+
+  if(comentario.style.display === 'inline'){
+      comentario.style.display='none';
+      btn_icon.innerHTML='1 likes';
+      btn_icon_2.innerHTML='1 respostas';
+  }
+  else{
+      comentario.style.display='inline';
+      btn_icon.innerHTML='4 likes';
+      btn_icon_2.innerHTML='4 respostas';
+  }
+}
+
